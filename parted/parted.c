fbc975ba97d99b11bf81794a2a19e2f38068166c
Matt Davis <mattdavis9@gmail.com>
2008-04-28 22:28:49 -0400

    Corrected memory leak when displaying partition flags.
    Deallocated memory allocated by partition_print_flags()
    (cherry picked from commit fcbf070239dc590db6a2aaf9d9e3608d2ff45204)

--- parted/parted.c
+++ parted/parted.c
@@ -1274,6 +1274,7 @@ do_print (PedDevice** dev)
         char*           size;
         const char*     name;
         char*           tmp;
+        char*           flags;
         wchar_t*        table_rendered;
 
         disk = ped_disk_new (*dev);
@@ -1473,7 +1474,9 @@ do_print (PedDevice** dev)
                                     str_list_append (row, name);
                             }
 
-                            str_list_append (row, partition_print_flags (part));
+                            flags = partition_print_flags (part);
+                            str_list_append (row, flags);
+                            ped_free (flags);
                     } else {
                             if (has_extended)
                                     str_list_append (row, "");
